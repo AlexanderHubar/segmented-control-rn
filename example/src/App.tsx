9d490165b7923360910b6d54ba9638186b4e40c7
Oleksandr Hubar <oleksandr.hubar@ekreative.com>
2021-12-17 15:53:00 +0200

    feat: add default text color

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,29 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import SegmentedControl from 'segmented-control-rn';
-import { useState } from 'react';
 
 const INACTIVE_COLOR = 'rgba(0, 0, 0, 0.5)';
+const ACTIVE_COLOR = 'rgb(0, 0, 0)';
 
 const segments = [
   {
-    active: <Text style={{ color: INACTIVE_COLOR }}>First</Text>,
+    active: (
+      <Image
+        style={{ width: 20, height: 20, borderRadius: 10, margin: 10 }}
+        source={{
+          uri: 'https://avatars.githubusercontent.com/u/32979603?s=40&v=4',
+        }}
+      />
+    ),
     inactive: <Text style={{ color: INACTIVE_COLOR }}>First</Text>,
   },
   {
-    active: <Text>Second</Text>,
+    active: <Text style={{ color: ACTIVE_COLOR }}>Second</Text>,
     inactive: <Text style={{ color: INACTIVE_COLOR }}>Second</Text>,
   },
   {
-    active: <Text>Third</Text>,
+    active: <Text style={{ color: ACTIVE_COLOR }}>Third</Text>,
     inactive: <Text style={{ color: INACTIVE_COLOR }}>Third</Text>,
   },
 ];
